a0466d64b477c1d44fd6bf6f09f23f0db5a492e5
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2023-12-05 22:57:36 +0530

    styles: change font-variables in root-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,7 @@ import '@/styles/main.scss';
 import { Inter } from 'next/font/google';
 import { cookies } from 'next/headers';
 
-const inter = Inter({
-	subsets: ['latin'],
-	variable: '--font-sans',
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'Url shortener',
@@ -21,7 +18,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={`font-sans ${inter.variable}`}>
+			<body className={`${inter.className}`}>
 				<TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
 			</body>
 		</html>
