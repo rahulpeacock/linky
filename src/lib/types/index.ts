992a76b03bc28097822534e679ad3dfebb59e891
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2023-12-30 18:32:17 +0530

    build: update process.env types with .env.example file

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,19 +1,24 @@
 import { z } from 'zod';
 
 const processEnvSchema = z.object({
-	// Auth
-	NEXTAUTH_URL: z.string(),
+	// Server
+	SITE_URL: z.string(),
+
+	// Client
+	NEXT_PUBLIC_SITE_URL: z.string(),
+
+	// Drizzle
+	DATABASE_URL: z.string(),
+
+	// Next Auth
 	NEXTAUTH_SECRET: z.string(),
+	NEXTAUTH_URL: z.string(),
+
+	// Next Auth Providers
 	GOOGLE_CLIENT_ID: z.string(),
 	GOOGLE_CLIENT_SECRET: z.string(),
 	GITHUB_ID: z.string(),
 	GITHUB_SECRET: z.string(),
-
-	// server
-	DATABASE_URL: z.string(),
-
-	// Client
-	NEXT_PUBLIC_VERCEL_URL: z.string(),
 });
 processEnvSchema.parse(process.env);
 
