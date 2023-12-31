4b97c8ae0dd1c337a88f65611208b41b3a10346d
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2023-12-31 12:21:51 +0530

    feat and styles: responsive home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,11 @@ export default async function page() {
 			<main className='min-h-hvh'>
 				<section className='flex items-start justify-center py-14'>
 					<div className='max-w-maxi'>
-						<h2 className='text-center text-6xl font-semibold'>Shorten your long links :{')'}</h2>
-						<p className='max-w-xl mx-auto mt-4 text-center text-sm text-muted-foreground'>
+						<h2 className='text-center text-3xl sm:text-4xl md:text-6xl font-semibold'>Shorten your long links :{')'}</h2>
+						<p className='max-w-lg md:max-w-xl w-11/12 mx-auto mt-4 text-center text-sm text-muted-foreground'>
 							Linkly is an efficient and easy-to-use URL shortening service that streamlines your online experience.
 						</p>
-						<div className='text-center pt-5'>
+						<div className='text-center pt-4 md:pt-5'>
 							<Suspense fallback={<Skeleton className='h-9 w-36' />}>
 								<HeroAuth />
 							</Suspense>
